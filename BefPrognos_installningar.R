0b76d9e8d299b3416e306f8bf56a6f70c9918357
FaluPeppe <72673226+FaluPeppe@users.noreply.github.com>
2021-09-16 01:02:05 +0200

    Un-ignore inställningsfilen

---
+++ BefPrognos_installningar.R
@@ -0,0 +1,41 @@
+# Skript som anropar func_BefPrognos_syss.R för att skapa diagram och spara dem som .png-filer 
+
+library(httr)             # för att komma förbi brandväggen
+source("G:/Samhällsanalys/Automatisering och R/Skript/befprognos/func_BefPrognos.R", encoding = "utf-8", echo = FALSE)
+
+
+# ================================ Här gör vi inställningar för körningen ========================================
+aktlan <- "25"    #c("17", "20", "21")    # Välj län att göra prognoser på
+bara_lan <- FALSE                         # TRUE om bara län ska visas, FALSE för att visa länets kommuner
+AktuellRegion <- NULL                     # Till diagramrubriken - NULL om namnet ska hämtas automatiskt 
+jmfrtid <- 10                             # antal år i jämförelsen, alltså hur många års sikt vi vill titta på
+JmfrFleraPrognoser <- TRUE               # TRUE om vi vill jämföra med äldre prognoser, FALSE om vi bara vill se den senaste prognosen
+
+output_fold <- "G:/Samhällsanalys/API/Fran_R/Utskrift/"     # mapp på datorn som diagrammet skrivs till
+logga_path <- "G:/Samhällsanalys/MallarLoggor/logo_liggande_fri_svart.png"       # om vi vill ha logga med, annars kan vi ta bort denna rad eller ge variabeln värdet NULL
+diagram_capt <- "Källa: SCB:s befolkningsprognos\nBearbetning: Peter Möller, Region Dalarna"
+
+# Här skrivs url:erna till befolkningsprognostabellerna, om flera prognoser ska jämföras så läggs de 
+# som en vektor som kommer att loopas i funktionen, lägg den senaste url:en först i vektorn
+# det är bara den som används om man inte jämför prognoser med varandra
+
+url_tabeller <- c("http://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0401/BE0401A/BefProgOsiktRegN",
+                  "http://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0401/BE0401B/BefProgOsiktRegN20")
+
+
+# ================================== här körs funktionen =======================================
+
+# För att komma förbi brandvägg - om man har en sådan, annars kan man ta bort dessa två rader
+set_config(use_proxy(url = "http://mwg.ltdalarna.se", port = 9090, username = Sys.getenv("userid"), password = Sys.getenv("pwd")))
+set_config(config(ssl_verifypeer = 0L))
+
+# Och så kör vi själva funktionen
+SkapaBefPrognosDiagram(aktlan = aktlan, 
+                       bara_lan = bara_lan, 
+                       AktuellRegion = AktuellRegion,
+                       jmfrtid = jmfrtid, 
+                       JmfrFleraPrognoser = JmfrFleraPrognoser,
+                       url_tabeller = url_tabeller,
+                       output_fold = output_fold,
+                       logga_path = logga_path,
+                       diagram_capt = diagram_capt)
